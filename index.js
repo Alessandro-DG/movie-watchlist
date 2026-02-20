325b35270b19f17c83349af2bacf4af48338e82e
Alessandro DG <alessandrodigiacomantonio.dev@gmail.com>
2026-02-20 23:19:11 +0100

    chore: add and remove watchlist

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const omdbApiBaseUrl = "http://www.omdbapi.com/?apikey=6de98b2f&"
 const searchForm = document.querySelector(".search-form")
+let watchlist = localStorage.getItem("watchlist") ? JSON.parse(localStorage.getItem("watchlist")) : []
 
 searchForm.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -44,11 +45,17 @@ async function fetchMovies() {
                                 <div class="result-section-card-duration-genre-btn-container">
                                     <span class="result-section-card-duration">${movie.Runtime}</span>
                                     <span class="result-section-card-genre">${movie.Genre}</span>
-                                    <button class="add-to-watchlist-button">
+                                    <button class="add-to-watchlist-button ${watchlist.includes(movie.imdbID) ? "hidden" : ""}" data-imdbid="${movie.imdbID}">
                                         <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path fill-rule="evenodd" clip-rule="evenodd" fill="currentColor" d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM9 5C9 4.44772 8.55228 4 8 4C7.44772 4 7 4.44772 7 5V7H5C4.44772 7 4 7.44771 4 8C4 8.55228 4.44772 9 5 9H7V11C7 11.5523 7.44772 12 8 12C8.55228 12 9 11.5523 9 11V9H11C11.5523 9 12 8.55228 12 8C12 7.44772 11.5523 7 11 7H9V5Z" />
                                         </svg>
                                         Watchlist
+                                    </button>
+                                    <button class="remove-from-watchlist-button ${watchlist.includes(movie.imdbID) ? "" : "hidden"}" data-imdbid="${movie.imdbID}">
+                                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM5 7C4.44772 7 4 7.44772 4 8C4 8.55228 4.44772 9 5 9H11C11.5523 9 12 8.55229 12 8C12 7.44772 11.5523 7 11 7H5Z" fill="#111827"/>
+                                        </svg>
+                                        Remove
                                     </button>
                                 </div>
                                 <p class="result-section-card-plot">${movie.Plot}</p>
@@ -80,3 +87,14 @@ async function fetchMovieInfo(imdbID) {
     const res = await fetch(omdbApiBaseUrl + "i=" + imdbID)
     return await res.json()
 }
+
+document.addEventListener("click", (event) => {
+    if(event.target.classList.contains("add-to-watchlist-button")) {
+        if(watchlist.includes(event.target.dataset.imdbid)) {
+            alert("Movie already added to watchlist")
+            return
+        }
+        watchlist.push(event.target.dataset.imdbid)
+        localStorage.setItem("watchlist", JSON.stringify(watchlist))
+    }
+})
