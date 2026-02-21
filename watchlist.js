cd83844b3757e41af3699d566aba6a916d2e3787
Alessandro DG <alessandrodigiacomantonio.dev@gmail.com>
2026-02-21 12:09:50 +0100

    chore: watchlist page

--- watchlist.js
+++ watchlist.js
@@ -1,30 +1,17 @@
 const omdbApiBaseUrl = "https://www.omdbapi.com/?apikey=6de98b2f&"
-const searchForm = document.querySelector(".search-form")
-let watchlist = localStorage.getItem("watchlist") ? JSON.parse(localStorage.getItem("watchlist")) : []
+let watchlist = localStorage.getItem("watchlist")
+                            ? JSON.parse(localStorage.getItem("watchlist"))
+                            : []
+fetchMoviesFromLocalStorage()
 
-searchForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    fetchMovies()
-
-})
-
-async function fetchMovies() {
+async function fetchMoviesFromLocalStorage() {
     const resultsPlaceholder = document.querySelector(".results-section-placeholder")
     const resultsContainer = document.querySelector(".result-section-card-container")
-    const searchInput = document.querySelector(".search-input")
-    const res = await fetch(omdbApiBaseUrl + "s=" + searchInput.value)
 
-    if(!res.ok) {
-        resultsPlaceholder.innerHTML = "<p>The service is currently unavailable. Please try again later.</p>"
-        resultsPlaceholder.classList.remove("hidden")
-        return
-    }
-
-    const data = await res.json()
-    if(data.Response === "True") {
+    if(watchlist.length > 0) {
         let cardsHtml = ""
-        data.Search.forEach(item => {
-            fetchMovieInfo(item.imdbID)
+        watchlist.forEach(imdbID => {
+            fetchMovieInfo(imdbID)
                 .then(movie => {
                     if(movie.imdbID) {
                         cardsHtml += `
@@ -45,13 +32,7 @@ async function fetchMovies() {
                                 <div class="result-section-card-duration-genre-btn-container">
                                     <span class="result-section-card-duration">${movie.Runtime}</span>
                                     <span class="result-section-card-genre">${movie.Genre}</span>
-                                    <button class="add-to-watchlist-button ${watchlist.includes(movie.imdbID) ? "hidden" : ""}" data-imdbid="${movie.imdbID}">
-                                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path fill-rule="evenodd" clip-rule="evenodd" fill="currentColor" d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM9 5C9 4.44772 8.55228 4 8 4C7.44772 4 7 4.44772 7 5V7H5C4.44772 7 4 7.44771 4 8C4 8.55228 4.44772 9 5 9H7V11C7 11.5523 7.44772 12 8 12C8.55228 12 9 11.5523 9 11V9H11C11.5523 9 12 8.55228 12 8C12 7.44772 11.5523 7 11 7H9V5Z" />
-                                        </svg>
-                                        Watchlist
-                                    </button>
-                                    <button class="remove-from-watchlist-button ${watchlist.includes(movie.imdbID) ? "" : "hidden"}" data-imdbid="${movie.imdbID}">
+                                    <button class="remove-from-watchlist-button" data-imdbid="${movie.imdbID}">
                                         <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path fill-rule="evenodd" clip-rule="evenodd" d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM5 7C4.44772 7 4 7.44772 4 8C4 8.55228 4.44772 9 5 9H11C11.5523 9 12 8.55229 12 8C12 7.44772 11.5523 7 11 7H5Z" fill="#111827"/>
                                         </svg>
@@ -77,7 +58,15 @@ async function fetchMovies() {
 
         })
     } else {
-        resultsPlaceholder.innerHTML = "<p>Unable to find what you’re looking for. Please try another search.</p>"
+        resultsPlaceholder.innerHTML = `
+            <p>Your watchlist is looking a little empty...</p>
+            <a href="./index.html" class="add-watchlist-link">
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" fill="currentColor" d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM9 5C9 4.44772 8.55228 4 8 4C7.44772 4 7 4.44772 7 5V7H5C4.44772 7 4 7.44771 4 8C4 8.55228 4.44772 9 5 9H7V11C7 11.5523 7.44772 12 8 12C8.55228 12 9 11.5523 9 11V9H11C11.5523 9 12 8.55228 12 8C12 7.44772 11.5523 7 11 7H9V5Z" />
+                </svg>
+                Let’s add some movies!
+            </>
+        `
         resultsPlaceholder.classList.remove("hidden")
         resultsContainer.innerHTML = ""
     }
@@ -89,26 +78,16 @@ async function fetchMovieInfo(imdbID) {
 }
 
 document.addEventListener("click", (event) => {
-    if(event.target.classList.contains("add-to-watchlist-button")) {
-        if(watchlist.includes(event.target.dataset.imdbid)) {
-            alert("Movie already added to watchlist")
-            return
-        }
-        watchlist.push(event.target.dataset.imdbid)
-        localStorage.setItem("watchlist", JSON.stringify(watchlist))
-        event.target.classList.add("hidden")
-        event.target.nextElementSibling.classList.remove("hidden")
-    }
-
     if(event.target.classList.contains("remove-from-watchlist-button")) {
-        if(! watchlist.includes(event.target.dataset.imdbid)) {
-            alert("Movie already removed to watchlist")
-            return
-        }
-        watchlist = watchlist.filter(imdbId => imdbId !== event.target.dataset.imdbid)
+        console.log(event.target.dataset.imdbid)
         console.log(watchlist)
-        localStorage.setItem("watchlist", JSON.stringify(watchlist))
-        event.target.classList.add("hidden")
-        event.target.previousElementSibling.classList.remove("hidden")
+        console.log(watchlist.includes(event.target.dataset.imdbid))
+        if(watchlist.includes(event.target.dataset.imdbid)) {
+            watchlist = watchlist.filter(imdbId => imdbId !== event.target.dataset.imdbid)
+            localStorage.setItem("watchlist", JSON.stringify(watchlist))
+            fetchMoviesFromLocalStorage()
+        } else {
+            alert("Movie already removed to watchlist")
+        }
     }
 })
